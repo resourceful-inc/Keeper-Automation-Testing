c1c6071ae42d5cdf573b4f1832cd64ab30f9f78c
acvan11 <anvanc37@gmail.com>
2019-07-08 20:44:32 -0700

    Automation test cases for Sign In

---
+++ TC6_SignIn.java
@@ -0,0 +1,19 @@
+package keeper2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC6_SignIn {
+
+	public static void main(String[] args) {
+		// TODO candidate logs in with correct email and password
+		System.setProperty("webdriver.chrome.driver", "/Users/sky/Documents/workspace/Selenium/src/test1/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://test.keeperhr.com/");
+		driver.findElement(By.id("email")).sendKeys("uongbanhman@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("12345678");
+		driver.findElement(By.xpath("//*[@id='main']/div/div[2]/div[3]/form/div[2]/button[1]")).click();
+	}
+
+}
