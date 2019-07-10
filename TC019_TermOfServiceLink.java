21b11e991b211f196b3cf9e57cf7b49aeeedc618
acvan11 <anvanc37@gmail.com>
2019-07-09 19:30:52 -0700

    finish "Terms of service" test case

--- TC019_TermOfServiceLink.java
+++ TC019_TermOfServiceLink.java
@@ -4,16 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TC018_WhatIsKeeperLink {
+public class TC019_TermOfServiceLink {
 
 	public static void main(String[] args) {
 		//*Test Case:*
-		//Summary: User clicks on "What is Keeper?" link
-		//Expected Result: It will direct user to website www.keeperhr.com
+		//Summary: Candidate clicks on "Term of Service" link
+		//Expected Result: It will direct candiate to "Term of Service" page
 		System.setProperty("webdriver.chrome.driver", "/Users/sky/Documents/workspace/Selenium/src/test1/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://test.keeperhr.com/");
-		driver.findElement(By.linkText("What is Keeper?")).click();
+		driver.findElement(By.xpath("//*[@id='main']/div/div[2]/div[3]/form/div[2]/button[2]")).click();
+		driver.findElement(By.linkText("Terms of Service")).click();
 	}
 
 }
